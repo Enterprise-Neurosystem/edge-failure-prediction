e4644f3e67921f68de5f40afe4f6fdc6e2613d53
guiderae <guiderae@yahoo.com>
2022-08-11 13:01:49 -0700

    Changed cache dir to static/cache.  Flask does not allow making a dir outside static.  Do not enable test tab and train tab if data is present in cache.

--- managers/train_manager.py
+++ managers/train_manager.py
@@ -28,8 +28,8 @@ class TrainManager:
         # TODO:  make callbacks configurable
         callback = LearningRateScheduler(TrainManager.scheduler)
         history = TrainManager.model.fit(X, y, epochs, batch_size, callbacks=[callback])
-        TrainManager.model.save('tests/trained_model')
-        #joblib.dump(TrainManager.model, 'tests/trained_model')
+        TrainManager.model.save('static/cache/trained_model')
+        #joblib.dump(TrainManager.model, 'static/cache/trained_model')
 
         return history
 
@@ -38,5 +38,5 @@ class TrainManager:
         return epochs * batch_size
 
    # def save_model(self):
-        # joblib.dump(self.model, 'tests/trained_model.gz')
-   #     tf.keras.models.save_model(self.model, 'tests/trained_model.gz')
+        # joblib.dump(self.model, 'static/cache/trained_model.gz')
+   #     tf.keras.models.save_model(self.model, 'static/cache/trained_model.gz')
