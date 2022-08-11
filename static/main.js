8110f941ef41559760ded8377dd10f173f69a47f
guiderae <guiderae@yahoo.com>
2022-08-11 12:50:52 -0700

    Changed cach dir to static/cache.  Flask does not allow making a dir outside static.  Do not enable test tab and train tab if data is present in cache.

--- static/main.js
+++ static/main.js
@@ -37,16 +37,16 @@ async function doTwoFilesExist(fileName1, fileName2){
 // When Data Prep tab is clicked, first determined if model has already been trained and scaler exists.
 // If both exist, enable Train Tab, Test Tab and Prediction Tab
 function onloadDataCheck(){
-    let fileName1 = 'tests/trained_model/saved_model.pb';
-    let fileName2 = 'tests/training_scaler.gz'
+    let fileName1 = 'static/cache/trained_model/saved_model.pb';
+    let fileName2 = 'static/cache/training_scaler.gz'
     let dataPresent = doTwoFilesExist(fileName1, fileName2); //This is a Promise that was returned from the
                                                              //async function, doTwoFilesExist()
 
     dataPresent.then(function(result){  // The variable, dataPresent is a Promise,
     if(result){
         predTabBtn.disabled = false;
-        testTabBtn.disabled = false;
-        trainTabBtn.disabled = false;
+        //testTabBtn.disabled = false;
+        //trainTabBtn.disabled = false;
     }else{
         predTabBtn.disabled = true;  // This could be redundant since when page loads, the btn is disabled.
         testTabBtn.disabled = true;
