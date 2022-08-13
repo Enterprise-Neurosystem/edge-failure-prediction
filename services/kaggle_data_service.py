dce70f0c3f083419ff4027065360398173064d6c
guiderae <guiderae@yahoo.com>
2022-08-13 08:56:19 -0700

    New class to manage Postgres 'originalkaggle' table

---
+++ services/kaggle_data_service.py
@@ -0,0 +1,24 @@
+from services.connection_pool_singleton import ConnectionPoolSingleton
+import pandas as pd
+import psycopg2
+
+class KaggleDataService:
+    GET_ALL_ROWS = 'Select * from originalkaggle order by timestamp'
+
+    @staticmethod
+    def get_all_as_df():
+        pool = ConnectionPoolSingleton.getConnectionPool()
+        connection = pool.getconn()
+        try:
+            with connection:
+                df = pd.read_sql_query(KaggleDataService.GET_ALL_ROWS, connection)
+                df.set_index('timestamp', inplace=True)
+        except (Exception, psycopg2.DatabaseError) as err:
+            print(err)
+        else:
+            return df
+        finally:
+            pool.putconn(connection)
+
+
+
