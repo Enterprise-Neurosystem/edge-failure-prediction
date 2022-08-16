3fce09bf22bc213548a495eea91d62bbd343f147
guiderae <guiderae@yahoo.com>
2022-08-16 12:50:29 -0700

    Added method, get_kafka_data()

--- dataprep/data_source_manager.py
+++ dataprep/data_source_manager.py
@@ -1,5 +1,7 @@
 from csv import DictReader
 import time
+from services.kafka_data_service import KafkaDataService
+import json
 
 
 class DataSourceManager:
@@ -23,7 +25,26 @@ class DataSourceManager:
                 yield row
 
     @staticmethod
-    def get_kafka_data():
+    def get_kafka_data(group_id):
         """
         Create a generator that yields one row at a time of raw sensor data
         """
+        kafka_data_service = KafkaDataService()
+        consumer = kafka_data_service.get_Kafka_consumer()
+        # sensor_name_list = KafkaDataService.sensor_names_list
+        try:
+            for record in consumer:
+                msg = record.value.decode("utf-8")
+                topic = record.topic
+                msg_list = list(json.loads(msg).values())
+                id = int(record.key)
+                msg_dict = kafka_data_service.message_to_dict(msg_list)
+                print(id, msg_dict)
+                # do something with message (display in web UI, send to database)
+                if id == group_id:
+                    yield msg_dict
+
+        finally:
+            print("Closing consumer...")
+            consumer.close()
+        print("Kafka consumer stopped.")
