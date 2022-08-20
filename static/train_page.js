d340381e7d05f1f011ffedf259f657a31b1c3e82
guiderae <guiderae@yahoo.com>
2022-08-20 14:55:58 -0700

    Added another set of async functions to handle generation of the graph only.  Added appropriate disable and enable code for the two buttons.

--- static/train_page.js
+++ static/train_page.js
@@ -1,14 +1,16 @@
+
+// Once model has been trained, display the loss graph.
 function trainGraph(){
     //console.log("trainGraph()");
-    graphDivObj.innerHTML = "<img src='static/TrainMsg.png'/>";
-    let url = '/train-model';
+    makeGraphBtnObj.disabled = "true";
+    let url = '/display-train-graph';
     let formObj = document.getElementById("trainForm")
     let formData = new FormData(formObj);
-    postTrainRequest(url, formData)
+    postTrainGraphRequest(url, formData)
         .then(serializedImage => displayTrainGraphHTML(serializedImage))
         .catch(error => console.error(error))
 }
-async function postTrainRequest(url, formData) {
+async function postTrainGraphRequest(url, formData) {
     return fetch(url, {
         method: 'POST',
         body: formData
@@ -24,8 +26,29 @@ function displayTrainGraphHTML(serializedImage){
     testTabBtn.disabled = false;
     predTabBtn.disabled = false;
 }
-
-
+// Train model
+function trainModel(){
+    let url = '/train-model';
+    graphDivObj.innerHTML = "<img src='static/TrainMsg.png'/>";
+    let formObj = document.getElementById("trainForm")
+    let formData = new FormData(formObj);
+    postTrainRequest(url, formData)
+        .then(txtMsg => displayMsg(txtMsg))
+        . catch(error => console.error(error))
+}
+async function postTrainRequest(url, formData){
+    return fetch(url,{
+        method: 'POST',
+        body: formData
+    })
+        .then((response) => response.text());
+}
+function displayMsg(txtMsg){
+    graphDivObj.innerHTML = txtMsg;
+    makeGraphBtnObj.disabled = false;
+}
 const graphDivObj = document.getElementById("trainGraphDiv");
-const makeGraphBtnObj = document.getElementById("trainingBtn");
+const makeGraphBtnObj = document.getElementById("trainGraphBtn");
+const trainBtnObj = document.getElementById("trainingBtn");
 makeGraphBtnObj.addEventListener("click", trainGraph);
+trainBtnObj.addEventListener('click', trainModel);
